ea33c89dfca033da38a470277e3cec4c3d5d7117
Alexander Pinnecke <alexander.pinnecke@googlemail.com>
2019-11-25 16:46:39 +0100

    WIP

--- go.mod
+++ go.mod
@@ -8,4 +8,6 @@ require (
 	github.com/prometheus/client_golang v1.2.1
 	github.com/robfig/cron/v3 v3.0.0
 	github.com/sirupsen/logrus v1.4.2
+	k8s.io/apimachinery v0.0.0-20191028221656-72ed19daf4bb
+	k8s.io/client-go v0.0.0-20191114101535-6c5935290e33
 )
--- main.go
+++ main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"os/user"
+	"path/filepath"
 
 	"github.com/gorilla/mux"
 	"github.com/kelseyhightower/envconfig"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/sirupsen/logrus"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/fielmann-ag/ops-version-monitor/pkg/html"
 	"github.com/fielmann-ag/ops-version-monitor/pkg/version"
@@ -19,8 +26,10 @@ var (
 )
 
 type envConfig struct {
-	Listen string `default:":8080"`
-	Verbose  bool
+	Listen      string `default:":8080"`
+	Verbose     bool
+	Kubeconfig  string
+	KubeContext string
 }
 
 func init() {
@@ -38,6 +47,20 @@ func main() {
 	monitor := version.NewPeriodicMonitor(logger)
 	if err := monitor.Start(); err != nil {
 		logger.Fatal(err)
+	}
+
+	cfg, err := loadKubernetesClientConfig()
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	client := kubernetes.NewForConfigOrDie(cfg)
+	deps, err := client.AppsV1().Deployments("").List(metav1.ListOptions{})
+	if err != nil {
+		logger.Fatalf("failed to load deployment list: %v", err)
+	}
+	for _, dep := range deps.Items {
+		logger.Debug(dep.GetNamespace() + "/" + dep.GetName() + ": " + dep.Spec.Template.Spec.Containers[0].Image)
 	}
 
 	logger.Printf("Listening on %s", config.Listen)
@@ -58,4 +81,37 @@ func router(monitor version.Monitor, logger *logrus.Logger) http.Handler {
 	})
 
 	return r
+}
+
+// loadKubernetesClientConfig loads a REST Config as per the rules specified in GetConfig
+// stolen from: https://github.com/kubernetes-sigs/controller-runtime/blob/2fe837fb5b0f4cfa9e566aa1027196a817692581/pkg/client/config/config.go
+func loadKubernetesClientConfig() (*rest.Config, error) {
+	if len(config.Kubeconfig) > 0 {
+		return loadConfigWithContext(config.Kubeconfig)
+	}
+
+	if len(os.Getenv("KUBECONFIG")) > 0 {
+		return loadConfigWithContext(os.Getenv("KUBECONFIG"))
+	}
+
+	if c, err := rest.InClusterConfig(); err == nil {
+		return c, nil
+	}
+
+	if usr, err := user.Current(); err == nil {
+		if c, err := loadConfigWithContext(filepath.Join(usr.HomeDir, ".kube", "config")); err == nil {
+			return c, nil
+		}
+	}
+
+	return nil, fmt.Errorf("could not locate a kubeconfig")
+}
+
+func loadConfigWithContext(kubeconfig string) (*rest.Config, error) {
+	return clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+		&clientcmd.ClientConfigLoadingRules{ExplicitPath: kubeconfig},
+		&clientcmd.ConfigOverrides{
+			CurrentContext: config.KubeContext,
+		},
+	).ClientConfig()
 }
